a8ef08f3890f1e7407a5e41856ad85e72a0519bf
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-01-02 01:16:48 +0530

    image validation with mb and kb

--- api/forms.py
+++ api/forms.py
@@ -11,11 +11,19 @@ VALID_IMAGE_EXTENSIONS = [
 ]
 
 
+def validate_file_size(value):
+    filesize= value.size
+    
+    if filesize > 1024:
+        raise ValidationError("You cannot upload file more than Mb")
+    else:
+        return value
+
 
 
 class UploadFileForm3(forms.Form):
      fileimg = forms.FileField(
-           validators=[FileExtensionValidator(VALID_IMAGE_EXTENSIONS)]
+           validators=[FileExtensionValidator(VALID_IMAGE_EXTENSIONS),validate_file_size],
                       
         )
 
